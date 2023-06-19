00be1ea47b8851b1daa7130ec3410424f9134682
Zazhigalka <mamiev.sultan06@gmail.com>
2023-06-19 19:37:52 +0600

    new folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import HomePage from "./components/HomePage/Home";
+import NavbarPage from "./pages/NavbarPage";
+import FooterPage from "./pages/FooterPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <NavbarPage />
+      <HomePage />
+      <FooterPage />
+    </div>
+  );
 };
 
 export default App;
